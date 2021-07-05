08d460ad7e8587fcdf7fadc495471b7b9767207b
ke-1 <jacobbodell@gmail.com>
2021-07-05 17:24:24 -0500

    started on cuboid inits

--- SANDSEA/engine/3space/objects/cuboid.py
+++ SANDSEA/engine/3space/objects/cuboid.py
@@ -2,8 +2,7 @@ class cuboid:
     def __init__(self, pos1, pos2, behavior):
         print("[sandsea/engine/3space/objects/cuboid.py] Initializing cuboid ...", end="")
         self.behavior = behavior
-        if (type(pos1) == type((1, 2, 3)) and type(pos2) == type((1, 2, 3))) == False:
+        tupleExampTV = (1, 2, 3)
+        if (type(pos1) == type(tupleExampTV)) and (type(pos2) == type(tupleExampTV)) == False:
             print("ERROR: Positions not passed in as coordinates.")
             exit()
-        else:
-            
